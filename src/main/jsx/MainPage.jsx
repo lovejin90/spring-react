f505c0b80abb166ecdfdd2046ab4763fd06058e6
unknown <lovejin90>
2018-11-30 09:58:57 +0900

    array,form add

--- src/main/jsx/MainPage.jsx
+++ src/main/jsx/MainPage.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '../webapp/css/custom.css';
-import MyName from './MyName';
 import Counter from './Counter';
+import MyName from './MyName';
+import PhoneForm from './PhoneForm';
+import PhoneInfoList from './PhoneInfoList';
  
  
 class MainPage extends React.Component {
- 
+    id = 2
+    state = {
+        information: [
+        {
+            id: 0,
+            name: '김민준',
+            phone: '010-0000-0000'
+        },
+        {
+            id: 1,
+            name: '홍길동',
+            phone: '010-0000-0001'
+        }
+        ]
+    }
+    handleCreate = (data) => {
+        const { information } = this.state;
+        this.setState({
+          information: information.concat({ id: this.id++, ...data })
+        })
+      }
     render() {
+        const { information } = this.state;
         const style = {
             backgroundColor: 'black',
             padding: '16px',
@@ -21,7 +44,12 @@ class MainPage extends React.Component {
                 </div>
                 <div className="test">
                     <MyName  />
-                    <Counter  />
+                    <Counter/>
+                    <PhoneForm 
+                      onCreate={this.handleCreate}/>
+                    {JSON.stringify(information)}
+
+                    <PhoneInfoList data={this.state.information}/>
                 </div>
             </div>
         );
